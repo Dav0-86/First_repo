2d200d73c0c8ce66607dc1b9238c472ce7e1f0d5
David Sukiasyan <david2003@ukr.net>
2024-01-23 14:55:53 +0200

    first commit

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print ("Hello World")
-print("Hello Git")
+import datetime
+
+
+date1 = input("Введіть дату у форматі yyyy-mm-dd: ")
+
+try:
+    date_input = datetime.datetime.strptime(date1, "%Y-%m-%d")
+
+except Exception:
+    print("Помилка: дата має бути у форматі yyyy-mm-dd")
+
+
+def get_days_from_today(date):
+    date_now = datetime.datetime.today()
+    if date <= date_now:
+        difference_time = date_now - date_input
+    else:
+        difference_time = -(date_input - date_now)
+    return difference_time.days
+    
+print(get_days_from_today(date_input))
