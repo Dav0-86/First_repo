531e169efb25e3ad6378cb86511bfd84751941ec
Dav0-86 <153777770+Dav0-86@users.noreply.github.com>
2024-01-30 13:47:15 +0200

    Create goit-algo-wh-04-02.py

---
+++ goit-algo-wh-04-02.py
@@ -0,0 +1,24 @@
+
+text = '''60b90c1c13067a15887e1ae1,Tayson,3
+60b90c2413067a15887e1ae2,Vika,1
+60b90c2e13067a15887e1ae3,Barsik,2
+60b90c3b13067a15887e1ae4,Simon,12
+60b90c4613067a15887e1ae5,Tessi,5'''
+
+
+with open("D:\\cats_file.txt", "w", encoding="utf-8") as file:
+    file.write(text) 
+
+outlist = []
+
+def get_cats_info(path):
+    with open("D:\\cats_file.txt", "r", encoding="utf-8") as file:
+        for split in text.split("\n"):
+            split = split.split(",")
+            outlist.append({'id': split[0], 'name': split[1], 'age': split[2]})
+        return outlist
+
+
+
+cats_info = get_cats_info("D:\\cats_file.txt")
+print(cats_info)
