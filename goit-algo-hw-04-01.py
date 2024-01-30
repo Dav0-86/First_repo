1ad797c5aa3d8a598c822a748ed3bbb138a6d5c8
David Sukiasian <david2003@ukr.net>
2024-01-30 19:12:54 +0200

    update

--- goit-algo-hw-04-01.py
+++ goit-algo-hw-04-01.py
@@ -1,20 +1,22 @@
+
+
 text = '''Alex Korp,3000
 Nikita Borisenko,2000
 Sitarama Raju,1000'''
 
-with open("D:\\salary_file.txt", "w", encoding="utf-8") as file:
-    file.write(text) 
+with open("D:\\Oplata_truda.txt", "w", encoding="utf-8") as file:
+    file.write(text)
+
 def total_salary(path):
-    total = 0
-    count = 0
-
-    with open("D:\\salary_file.txt", "r", encoding="utf-8") as file:
+    total_cash = 0
+    average_salary = 0
+    with open("D:\\Oplata_truda.txt", "r", encoding="utf-8") as file:
         for line in file:
-            salary = line.split(",")[1]
-            total += int(salary)
-            count += 1
-    average = total / count
-    return (total, average)
-
-total, average = total_salary("D:\\salary_file.txt")
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+            salary = line.split(",")
+            total_cash = total_cash + int(salary[1])
+            average_salary = average_salary + 1
+        average = total_cash / average_salary
+        return (total_cash, average)
+    
+total_cash, average = total_salary("D:\\Oplata_truda.txt")
+print(f"Загальна сума заробітної плати: {total_cash}, Середня заробітна плата: {average}")
