5689a8453c699fcbcf55358c483dbc5ee3007101
Dav0-86 <153777770+Dav0-86@users.noreply.github.com>
2024-02-03 20:41:39 +0200

    Update goit-algo-hw-04-01.py

--- goit-algo-hw-04-01.py
+++ goit-algo-hw-04-01.py
@@ -1,3 +1,4 @@
+
 text = '''Alex Korp,3000
 Nikita Borisenko,2000
 Sitarama Raju,1000'''
@@ -13,13 +14,16 @@ except Exception as error:
 def total_salary(path):
     total_cash = 0
     average_salary = 0
-    with open("D:\\Oplata_truda.txt", "r", encoding="utf-8") as file:
-        for line in file:
-            salary = line.split(",")
-            total_cash = total_cash + int(salary[1])
-            average_salary = average_salary + 1
-        average = total_cash / average_salary
-        return (total_cash, average)
+    try:
+        with open("D:\\Oplata_truda.txt", "r", encoding="utf-8") as file:
+            for line in file:
+                salary = line.split(",")
+                total_cash = total_cash + int(salary[1])
+                average_salary = average_salary + 1
+            average = total_cash / average_salary
+            return (total_cash, average)
+    except Exception as error:
+        print(f"Виникла помилка --> {error}")
     
 total_cash, average = total_salary("D:\\Oplata_truda.txt")
 print(f"Загальна сума заробітної плати: {total_cash}, Середня заробітна плата: {average}")
